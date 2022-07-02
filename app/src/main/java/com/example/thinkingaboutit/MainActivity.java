1969fb47ddebb7c246d6ce03e75138d41efcd320
Brianhulela <101281239+Brianhulela@users.noreply.github.com>
2022-07-02 16:10:09 +0200

    added actionbar

--- app/src/main/java/com/example/thinkingaboutit/MainActivity.java
+++ app/src/main/java/com/example/thinkingaboutit/MainActivity.java
@@ -3,18 +3,22 @@ package com.example.thinkingaboutit;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
     MaterialButton likeButton;
-    ConstraintLayout likeButtonsLayout;
+    ImageButton bookmarkButton;
     boolean like = true;
+    boolean bookmark = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         likeButton = findViewById(R.id.likeButton);
-        likeButtonsLayout = findViewById(R.id.likeButtonsLayout);
+        bookmarkButton = findViewById(R.id.bookmarkImagebutton);
         Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.popup_animation);
 
         likeButton.setOnClickListener(new View.OnClickListener() {
@@ -30,11 +34,30 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (like){
                     //likeButtonsLayout.startAnimation(animation);
-                    likeButtonsLayout.setVisibility(View.VISIBLE);
+                    likeButton.setBackgroundColor(Color.parseColor("#66BB6A"));
                     like = false;
                 }else {
-                    likeButtonsLayout.setVisibility(View.GONE);
+                    likeButton.setBackgroundColor(Color.parseColor("#A3A3A3"));
                     like = true;
+                }
+            }
+        });
+
+        bookmarkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (bookmark){
+                    String uri = "@drawable/ic_checked_bookmark";  // where myresource (without the extension) is the file
+                    int imageResource = getResources().getIdentifier(uri, null, getPackageName());
+                    Drawable res = getResources().getDrawable(imageResource);
+                    bookmarkButton.setImageDrawable(res);
+                    bookmark = false;
+                }else{
+                    String uri = "@drawable/ic_unchecked_bookmark";  // where myresource (without the extension) is the file
+                    int imageResource = getResources().getIdentifier(uri, null, getPackageName());
+                    Drawable res = getResources().getDrawable(imageResource);
+                    bookmarkButton.setImageDrawable(res);
+                    bookmark = true;
                 }
             }
         });
