c794eb9effe76ea917f3d37e3031370b6e3a5366
Brianhulela <101281239+Brianhulela@users.noreply.github.com>
2022-07-02 15:33:12 +0200

    added like buttons

--- app/src/main/java/com/example/thinkingaboutit/MainActivity.java
+++ app/src/main/java/com/example/thinkingaboutit/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.thinkingaboutit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    MaterialButton likeButton;
+    ConstraintLayout likeButtonsLayout;
+    boolean like = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        likeButton = findViewById(R.id.likeButton);
+        likeButtonsLayout = findViewById(R.id.likeButtonsLayout);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.popup_animation);
+
+        likeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (like){
+                    //likeButtonsLayout.startAnimation(animation);
+                    likeButtonsLayout.setVisibility(View.VISIBLE);
+                    like = false;
+                }else {
+                    likeButtonsLayout.setVisibility(View.GONE);
+                    like = true;
+                }
+            }
+        });
     }
 }
